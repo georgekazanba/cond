d70f3110e73c4e6d0c866aee7ec5b901f7f220ac
georgekazanba <90256349+georgekazanba@users.noreply.github.com>
2023-05-26 15:52:31 +0300

    Add files via upload

---
+++ js/consult.js
@@ -0,0 +1,16 @@
+var consult = document.querySelector('.consult');
+
+
+function openConsult() {
+    consult.classList.replace('modal', 'modal-active');
+    document.body.style.overflow = 'hidden';
+    console.log('а');
+}
+function closeConsult() {
+    consult.classList.replace('modal-active', 'modal');
+    document.body.style.overflow = '';
+    console.log('б');
+}
+document.addEventListener('keydown', function(e) {
+    e.code = "Escape" ? closeConsult() : '';
+});
